9e21cacca9271263362bf6b78ac2090d8e915e21
Azekowka <gabjtov@gmail.com>
2025-08-14 17:26:06 +0500

    tailwind, prettier, eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,7 +10,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier"
+  ),
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
